abb7f736383a15d10dcb450868cc7fefc30df1c5
Irina Giruta <irina.giruta@gmail.com>
2018-06-02 00:54:19 +0300

    Add files via upload
    add styles for .small section and .portfolio

---
+++ js/init.js
@@ -0,0 +1,13 @@
+$( function() {
+    // init tabs
+    $( ".services-tab" ).tabs();
+    // init portfolio tabs
+    $( ".portfolio-tab" ).tabs();
+
+    //init reviews carousel
+    $('.reviews-carousel').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+} );
